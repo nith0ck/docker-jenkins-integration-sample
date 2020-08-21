bf66799db98c33590ee042bc0b3c848b56ef74c9
romenige <romenigepinto@gmail.com>
2020-08-21 16:31:53 -0100

    test2

--- src/main/java/com/javatechie/docker_jenkins_integration_sample/Test.java
+++ src/main/java/com/javatechie/docker_jenkins_integration_sample/Test.java
@@ -1,9 +1,42 @@
 package com.javatechie.docker_jenkins_integration_sample;
 
+
+import java.util.Vector;
+import java.util.Enumeration;
+
 public class Test {
-	public static void main( String[] args )
-    {
-        System.out.println( "Hello!" );
+    Vector v = new Vector();
+    String submit = null;
+    String item = null;
+   
+    private void addItem(String name) {
+    v.addElement(name);
     }
-	
+
+    private void removeItem(String name) {
+    v.removeElement(name);
+    }
+
+    public void setItem(String name) {
+    item = name;
+    }
+   
+    public void setSubmit(String s) {
+    submit = s;
+    }
+
+    public String[] getItems() {
+    String[] s = new String[ v.size()];
+    v.copyInto(s);
+    return s;
+    }
+   
+ 
+
+    // reset
+    private void reset() {
+    submit = null;
+    item = null;
+    }
 }
+
