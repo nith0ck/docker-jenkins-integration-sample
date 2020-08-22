a459a902c2ed99dbbbac8c09de8d3b7a223a186c
romenige <romenigepinto@gmail.com>
2020-08-22 13:49:31 -0100

    commit 5

---
+++ src/main/java/com/javatechie/docker_jenkins_integration_sample/Library.java
@@ -0,0 +1,19 @@
+package com.javatechie.docker_jenkins_integration_sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Library {
+    public boolean someLibraryMethod() {
+    	return true;
+    }
+    
+    public void test() {
+    	List list = new ArrayList<String>();
+    	String input = null;
+    	String input1 = null;
+    	input1.length();
+    }
+}
+
